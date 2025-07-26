f349b3d2ce70fa824844ffec8961a16e89b95540
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2025-07-26 10:30:14 +0000

    v1 without proper working css

--- frontend/src/components/ui/sheet.tsx
+++ frontend/src/components/ui/sheet.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import * as React from "react";
 import * as SheetPrimitive from "@radix-ui/react-dialog@1.1.6";
-import { XIcon } from "lucide-react@0.487.0";
+import { XIcon } from "lucide-react";
+import * as React from "react";
 
 import { cn } from "./utils";
 
@@ -128,12 +128,7 @@ function SheetDescription({
 }
 
 export {
-  Sheet,
-  SheetTrigger,
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetFooter,
-  SheetTitle,
-  SheetDescription,
+  Sheet, SheetClose,
+  SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger
 };
+
